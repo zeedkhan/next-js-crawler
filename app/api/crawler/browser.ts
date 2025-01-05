8a2e38dd1adb9a55b1659a929f7d02bafb73925e
zeedkhan <zeedkhan.tp@gmail.com>
2025-01-05 13:49:34 +0800

    change duration

--- app/api/crawler/browser.ts
+++ app/api/crawler/browser.ts
@@ -2,30 +2,30 @@ const CHROMIUM_PATH = 'https://storage.googleapis.com/cralwer-files/chromium-v12
 
 async function getBrowser() {
     if (process.env.VERCEL_ENV === "production") {
-      const chromium = await import("@sparticuz/chromium-min").then(
-        (mod) => mod.default
-      );
-  
-      const puppeteerCore = await import("puppeteer-core").then(
-        (mod) => mod.default
-      );
-  
-      const executablePath = await chromium.executablePath(CHROMIUM_PATH);
-  
-      const browser = await puppeteerCore.launch({
-        args: chromium.args,
-        defaultViewport: chromium.defaultViewport,
-        executablePath,
-        headless: chromium.headless,
-      });
-      return browser;
+        const chromium = await import("@sparticuz/chromium-min").then(
+            (mod) => mod.default
+        );
+
+        const puppeteerCore = await import("puppeteer-core").then(
+            (mod) => mod.default
+        );
+
+        const executablePath = await chromium.executablePath(CHROMIUM_PATH);
+
+        const browser = await puppeteerCore.launch({
+            args: [...chromium.args, "--disable-extensions"],
+            defaultViewport: chromium.defaultViewport,
+            executablePath,
+            headless: chromium.headless,
+        });
+        return browser;
     } else {
-      const puppeteer = await import("puppeteer").then((mod) => mod.default);
-  
-      const browser = await puppeteer.launch();
-      return browser;
+        const puppeteer = await import("puppeteer").then((mod) => mod.default);
+
+        const browser = await puppeteer.launch();
+        return browser;
     }
-  }
+}
 
 export {
     getBrowser
--- app/api/crawler/route.ts
+++ app/api/crawler/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { getBrowser } from "./browser";
 import { parseMetaData } from "@/app/crawl/_component/data";
 
-export const dynamic = "force-dynamic";
+export const maxDuration = 30;
 
 export async function POST(request: NextRequest) {
     const body = await request.json();
