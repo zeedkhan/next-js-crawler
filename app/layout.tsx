f6ac3fe61dfefd7b17f0d0779b91a72469b95f74
zeedkhan <zeedkhan.tp@gmail.com>
2025-01-05 17:24:51 +0800

    adjust GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,9 +26,6 @@ const viewport: Viewport = {
   ],
 };
 
-
-const isProduction = process.env.NODE_ENV === "production";
-
 export default function RootLayout({
   children,
 }: {
@@ -37,7 +34,7 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning lang="en">
       <head />
-      <GoogleTagManager gtmId={!isProduction ? process.env.GTM_ID as string : "GTM-XYZ"} />
+      <GoogleTagManager gtmId={process.env.GTM_ID as string || "GTM-XYZ"} />
       <body
         className={clsx(
           "min-h-screen bg-background font-sans antialiased",
